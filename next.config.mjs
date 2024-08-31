feaaffd6f9bf31c268c3f9fcd72814d67e74ee7b
Zaryab <zaryabkhan248@gmail.com>
2024-08-31 18:04:37 +0500

    some config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  devIndicators: {
-    autoPrerender: false,
-  },
-  images: {
-    remotePatterns: [
-      {
-        hostname: "letsenhance.io",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
